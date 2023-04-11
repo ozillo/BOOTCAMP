7653c9b54eabbd063f0d04bf654ea04b33c7f87d
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-11 15:52:55 +0200

    Prueba

---
+++ JAVASCRIPT/Iteration2/iteration2.js
@@ -0,0 +1,6 @@
+//Iteración #2: Variables avanzadas
+//1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+//const character = {name: 'Jack Sparrow', age: 10};
+
+const character = {name: 'Jack Sparrow', age: 10'};
+
