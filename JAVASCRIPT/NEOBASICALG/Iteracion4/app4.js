97a28faf0a59228c59ec335707bc374162bc41be
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-13 00:46:13 +0200

    Iteracion4

---
+++ JAVASCRIPT/NEOBASICALG/Iteracion4/app4.js
@@ -0,0 +1,12 @@
+//1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0])
+
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"
+
+avengers[0]="IRONMAN"
+console.log(avengers)
+
+//1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+
