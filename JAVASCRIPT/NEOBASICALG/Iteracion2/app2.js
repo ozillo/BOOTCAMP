749e748ac0f3487891f200aaf90f947f3a7bc2ee
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-12 17:34:38 +0200

    iteracion214

--- JAVASCRIPT/NEOBASICALG/Iteracion2/app2.js
+++ JAVASCRIPT/NEOBASICALG/Iteracion2/app2.js
@@ -24,4 +24,17 @@ const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
 console.log(toy1.price+toy2.price)
-    
+
+//1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+//y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+//basePrice más el valor de la variable globalBasePrice.
+//let globalBasePrice = 10000;
+//const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+//const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+let globalBasePrice = 10000;
+const elements = [globalBasePrice= 25000]
+console.log(elements)
+
+
