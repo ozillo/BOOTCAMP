2f5479578dd977f3e941275205e3a9796f4419be
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-13 17:28:58 +0200

    Iteracion4

--- JAVASCRIPT/NEOBASICALG/Iteracion4/app4.js
+++ JAVASCRIPT/NEOBASICALG/Iteracion4/app4.js
@@ -10,3 +10,30 @@ console.log(avengers)
 
 //1.3 console numero de elementos en el array usando la propiedad correcta de Array.
 
+console.log(avengers.length)
+
+//1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+//Muestra en consola el último personaje del array
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push('Summer','Morty')
+console.log(rickAndMortyCharacters)
+
+//1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2)
+console.log(rickAndMortyCharacters2[0])
+console.log(rickAndMortyCharacters2[rickAndMortyCharacters2.length-1])
+
+
+
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters3.splice(1,1)
+console.log(rickAndMortyCharacters3)
+
+
+
+
