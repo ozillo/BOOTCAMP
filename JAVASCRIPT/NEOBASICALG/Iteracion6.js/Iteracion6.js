007f6273b14da70ef72ed6e600a4a75002afe3ea
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-19 14:09:03 +0200

    Iteracion6

---
+++ JAVASCRIPT/NEOBASICALG/Iteracion6.js/Iteracion6.js
@@ -0,0 +1,34 @@
+//Iteracion#6 Bucles
+
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+
+for (let x=0;x<=9;x++)
+{
+    console.log(x);
+}
+
+//1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+//cuando el resto del numero dividido entre 2 sea 0.
+
+for (let x=0;x<=9; x+=2){
+    
+console.log(x)
+}
+
+//1.3 Crea un bucle para conseguir dormir contando ovejas. 
+//Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+//Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+//y cambia el mensaje en la décima vuelta a 'Dormido!'.
+
+for (x=1;x<=10;x++) {
+
+if(x<=10){
+console.log("Intentando dormir 🐑")
+}
+else  (x=10){
+    console.log("Dormido!")
+
+}
+}
+
