1c4a694405a809380ff137c8aeedeb84b96a0431
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-21 07:35:43 +0200

    NEOLAND-BASIC-FUNCTIONS1

---
+++ JAVASCRIPT/NEOLAND-BASIC-FUNCTIONS/BF1.js
@@ -0,0 +1,15 @@
+//Iteración #1: Buscar el máximo
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne, numberTwo) {
+   if(numberOne<numberTwo){
+      return numberTwo
+      console.log("numberOne")
+   }
+   else {
+      console.log(numberTwo)
+      return numberOne
+   }  
+  
+}
+//En esta función le decimos que compare 4 y 7 y que retorne el numero mas alto que en este caso es 7 sinó  que retorne 4.
+
---
+++ JAVASCRIPT/NEOLAND-BASIC-FUNCTIONS/BF2.js
@@ -0,0 +1,34 @@
+//Iteración #2: Buscar la palabra más larga
+//Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+//Puedes usar este array para probar tu función:
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+  // insert code
+
+  function findLongestWord(param) {
+    let palabraMasLarga = ""
+    param.forEach((heroe, index)=> {
+      heroe.length > palabraMasLarga.length 
+      ? (palabraMasLarga = heroe) 
+      : (palabraMasLarga = palabraMasLarga)
+   })
+   console.log(palabraMasLarga)
+  }
+  
+  findLongestWord(avengers)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
