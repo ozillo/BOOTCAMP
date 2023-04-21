9e6bea75db3a48787679772e063c9030f2b88894
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-21 10:18:50 +0200

    Loops-Iteración6

---
+++ JAVASCRIPT/NEOLAND-LOOPS/loops6.js
@@ -0,0 +1,22 @@
+//Iteración #6: Mixed For...of e includes
+//Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato. 
+//Recuerda que puedes usar la función .includes() para comprobarlo.Puedes usar este array:
+
+
+const toys = [
+    {id: 5, name: 'Buzz MyYear'}, 
+    {id: 11, name: 'Action Woman'}, 
+    {id: 23, name: 'Barbie Man'}, 
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ]
+
+    let array = [];
+
+    for (let toy of toys){
+        
+         if (!(toy.name).includes("gato")){
+             array.push(toy);}     
+            
+    }
+    console.log(array);
