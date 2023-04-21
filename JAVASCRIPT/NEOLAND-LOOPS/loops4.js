5955660cd3302d4061e79c9fcc476b75659dc5b3
Marc Mateo Prieto <marcmateoprieto@MBP-de-Marc.home>
2023-04-21 09:48:59 +0200

    Loops-Iteracion4

---
+++ JAVASCRIPT/NEOLAND-LOOPS/loops4.js
@@ -0,0 +1,17 @@
+//Iteración #4: Probando For...in
+//Usa un for...in para imprimir por consola los datos del alienígena.. Puedes usar este objeto:
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+
+for(let key in alien){
+console.log(alien[key]);
+
+}
+
+//sacamos por pantalla todos los datos del alienigena
