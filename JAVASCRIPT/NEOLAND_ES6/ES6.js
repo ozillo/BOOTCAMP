d384e7c87f875c0c634b8d1424df975fac08128e
ozillo <marcmateo25@gmail.com>
2023-05-09 09:28:15 +0200

    ES6_Iteracion1

---
+++ JAVASCRIPT/NEOLAND_ES6/ES6.js
@@ -0,0 +1,17 @@
+//Crea una arrow function que tenga dos parametros a y b y 
+//que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+//por consola la suma de los dos parametros.
+
+//1.1 Ejecuta esta función sin pasar ningún parametro
+const suma = (a, b) => a+b
+console.log();
+
+
+//1.2 Ejecuta esta función pasando un solo parametro
+
+
+console.log(suma(10));
+
+//1.3 Ejecuta esta función pasando dos parametros
+
+console.log(suma(10,5));
